25563f3016df4b1b7d35c6fc85a982ed702ea936
Данила Деренченко <daniladerencenko@Danilas-MacBook-Pro.local>
2025-04-21 11:02:14 +0400

    initual

---
+++ src/app/components/package-card/package-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-package-card',
+  imports: [],
+  templateUrl: './package-card.component.html',
+  styleUrl: './package-card.component.scss'
+})
+export class PackageCardComponent {
+
+}
